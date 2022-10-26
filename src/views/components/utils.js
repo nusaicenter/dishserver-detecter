466449c54fce826c4d5763ca43c2861ed0408780
chujiahua <jiahua.chu@nusri.cn>
2022-10-27 01:08:54 +0800

    remove some files in wrong location

--- src/views/components/utils.js
+++
@@ -1,7 +0,0 @@
-const getImageObjKeys = (imgObj) => {
-    return imgObj.value.filter(e => e.select).map(e => e.key)
-}
-const getNameFromPath = (path) => {
-    return path.substr(path.lastIndexOf('/') + 1)
-}
-export { getImageObjKeys, getNameFromPath}
